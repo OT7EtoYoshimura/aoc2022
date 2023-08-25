a03fa229bac2e131d58d98122fa395050c886132
hanabi <hanabi@nomadalgia.xyz>
2023-08-25 18:49:05 +0200

    Finish d3p2; reorganize Day3.hs
    Signed-off-by: hanabi <hanabi@nomadalgia.xyz>

--- Day3.hs
+++ Day3.hs
@@ -2,12 +2,20 @@ module Day3 where
 
 import Data.Char
 import Data.List
+import Data.List.Split
 import Control.Applicative
 
-half xs = splitAt (length xs `div` 2) xs
-cartProd = uncurry $ liftA2 (,)
 priority x
   | isUpper x = ord x - 38
   | otherwise = ord x - 96
-parse = priority . fst . head . nub . filter (uncurry (==)) . cartProd . half
-p1 = sum . map parse . lines <$> readFile "d3"
+
+p1 = sum . map go . lines <$> readFile "d3" where
+  go = priority . fst . head . nub . filter (uncurry (==)) . cartProd . halves
+  halves xs  = splitAt (length xs `div` 2) xs
+  cartProd = uncurry $ liftA2 (,)
+
+p2 = sum . map go . chunksOf 3 . lines <$> readFile "d3" where
+  go = priority . head . foldr1 intersect
+
+-- 200 IQ alternative; although slower
+-- priority = succ . head . flip elemIndices (['a' .. 'z'] ++ ['A' .. 'Z'])
