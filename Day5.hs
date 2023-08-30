b640e9f87b8ef8be003c33d7e4f57dfae944b9ab
hanabi <hanabi@nomadalgia.xyz>
2023-08-30 20:19:53 +0200

    Add d5
    + I got some nice hands-on experience with lenses
    - The code still looks atrocious
    
    Signed-off-by: hanabi <hanabi@nomadalgia.xyz>

--- Day5.hs
+++ Day5.hs
@@ -4,8 +4,18 @@ import Data.Bifunctor
 import Data.Char
 import Data.List
 import Data.List.Extra
+import Data.List.Lens
+import Control.Lens
 
-p1     = bimap crates inst . breakOn "\n\n" <$> readFile "d5test"
+p1 = solve reverse
+p2 = solve id
+
+solve f = map head . uncurry (move f) . bimap crates inst . breakOn "\n\n" <$> readFile "d5"
+
+move _ crates [] = crates
+move f crates ([count, from, to]:rest) = move f newCrates rest where
+  taken = f $ take count $ (crates !! pred from)
+  newCrates = crates & over (ix $ pred from) (drop count) & over (ix $ pred to) (taken++)
+
 crates = filter (not . null) . map (filter isUpper) . splitOn "   "  . concat . transpose . lines . head . splitOn "\n 1"
-inst :: String -> [[Integer]]
-inst   = map (map read . filter (all isDigit) . words) . filter (not . null) . lines
+inst = map (map (read :: String -> Int) . filter (all isDigit) . words) . lines . drop 2
