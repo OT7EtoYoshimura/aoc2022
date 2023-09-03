84dd392b1e850e4f37e72ac3f8253b83c3914d1e
hanabi <hanabi@nomadalgia.xyz>
2023-09-03 20:49:22 +0200

    Make d8p1 by using mapAccum(L|R)
    Signed-off-by: hanabi <hanabi@nomadalgia.xyz>

--- Day8.hs
+++ Day8.hs
@@ -5,15 +5,11 @@ import Data.Char
 import Data.List
 import Data.Tuple.Extra
 
-p1 = trues . transOr . uncurry zip . second transpose . both (revOr . revPair) . transPair . digits . lines <$> readFile "d8" where
-  digits = map $ map digitToInt
-  transPair = toSnd transpose
-  revPair = map $ toSnd reverse
-  revOr = map $ zipOr . (visible *** (reverse . visible))
-  transOr = concatMap zipOr
-  trues = length . filter id
-  zipOr = uncurry $ zipWith (||)
-  toSnd f = id &&& f
-  visible = _visible (-1)
-  _visible max [] = []
-  _visible max (x:xs) = if x > max then True : _visible x xs else False : _visible max xs
+p1 = trues . transOr . both visible . transPair . digits . lines <$> readFile "d8" where
+  digits    = map $ map digitToInt
+  transPair = id &&& transpose
+  visible   = map $ liftM2 zipOr (vis mapAccumL) (vis mapAccumR)
+  transOr   = concatMap (uncurry zipOr) . uncurry zip . second transpose
+  trues     = length . filter id
+  zipOr     = zipWith (||)
+  vis f     = snd . f (\max x -> if (x > max) then (x, True) else (max, False)) (-1)
