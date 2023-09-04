28791489b664f570b1ae8108c6c6873cc4b7d3d1
hanabi <hanabi@nomadalgia.xyz>
2023-09-04 12:17:13 +0200

    Finish d8p2
    * Why does GHC.Utils.Misc shelter so many cool functions?
    
    Signed-off-by: hanabi <hanabi@nomadalgia.xyz>

--- Day8.hs
+++ Day8.hs
@@ -4,12 +4,20 @@ import Control.Monad
 import Data.Char
 import Data.List
 import Data.Tuple.Extra
+import GHC.Utils.Misc
 
-p1 = trues . transOr . both visible . transPair . digits . lines <$> readFile "d8" where
-  digits    = map $ map digitToInt
-  transPair = id &&& transpose
-  visible   = map $ liftM2 zipOr (vis mapAccumL) (vis mapAccumR)
-  transOr   = concatMap (uncurry zipOr) . uncurry zip . second transpose
-  trues     = length . filter id
-  zipOr     = zipWith (||)
-  vis f     = snd . f (\max x -> if (x > max) then (x, True) else (max, False)) (-1)
+p1 = length . filter id <$> solve (zipWith (||)) vis
+p2 = maximum            <$> solve (zipWith  (*)) scenic
+
+solve f g = transF f . both (revF f g) . transPair . digits . lines <$> readFile "d8"
+
+digits    = map $ map digitToInt
+transPair = id &&& transpose
+
+transF  f = concatMap (uncurry f) . uncurry zip . second transpose
+revF  f g = map $ liftM2 f (g mapAccumL) (g mapAccumR)
+
+vis     f = snd . f (\max x -> if (x > max) then (x, True) else (max, False)) (-1)
+
+scenic  f = snd . f (\his x -> (x:his, applyWhen (scenic' x his /= length his) succ $ scenic' x his)) []
+scenic' x = length . takeWhile (\y -> x > y)
